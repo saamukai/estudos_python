f6def0d5ce313eafe251896452db58358086583d
saamukai <contato.samukajpc@gmail.com>
2023-02-07 15:40:39 -0300

    desafio comparar matriz

--- listas_exercicios/serch_num.py
+++ listas_exercicios/serch_num.py
@@ -2,20 +2,26 @@
 
 # Função que encotnra o primeiro duplicado:
 matriz_inteiros = [
-    [1,2,3,4,5,6,7,8,9,10],
-    [9,1,8,9,9,7,2,1,5,8],
-    [1,3,2,2,8,6,5,9,6,7],
-    [8,2,8,6,2,4,5,6,1,7],
+    [1,2,3,4,5,2,7,8,3,10],
+#    [9,1,8,9,9,7,2,1,5,8],
+#    [1,3,2,2,8,6,5,9,6,7],
+#    [8,2,8,6,2,4,5,6,1,7],
 ]
 
 def search_first_duplicate(lista):
     iguais = []
-    indice = len(lista)
-    for indice, num in len(lista):
-        # if j >= 1:
-        #     if num[j] == num[j-1]:
-        #         iguais = iguais.append(num[j])
-        # print(iguais)
+
+    for compair in lista:
+        i = 0
+        for num in lista:
+            print(f'# {compair} x {num}')
+            if num == compair:
+                print(f'conta {i}')
+                i += 1
+
+    print(iguais)
+
+
 
     return lista
 
